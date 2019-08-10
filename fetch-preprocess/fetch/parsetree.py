152014c95f37d9aafc7918150aafbb26e7ffe9ef
mjlee98 <42081407+mjlee98@users.noreply.github.com>
2019-08-11 09:34:59 +1000

    Update parsetree.py

--- fetch-preprocess/fetch/parsetree.py
+++ fetch-preprocess/fetch/parsetree.py
@@ -3,9 +3,9 @@
 #  nor 3)
 from logicparser import *
 def generateParseTree(expression):
-    
+
     tree, vars_order = parse(expression)
-    print(tree)
+    #print(tree)
     return tree
 
-generateParseTree('(COMP2123|COMP2823)&COMP2017&(COMP2022|COMP2922)')
+#generateParseTree('(COMP2123|COMP2823)&COMP2017&(COMP2022|COMP2922)')
