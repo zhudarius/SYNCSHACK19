e39dbd33b353edbc680a6ff935b554cdd6d8add4
Victor Kuo <vck3000@gmail.com>
2019-08-10 22:12:05 +1000

    Finished all units, top row, full screen graph

--- server/assets/js/index.js
+++ server/assets/js/index.js
@@ -5,20 +5,35 @@ $(document).ready(() => {
 });
 
 // Send a POST request
-axios({
-    method: 'POST',
-    url: 'http://10.42.0.50:5000/unit_category',
-    data: {
-        units_taken: ["C", "D"]
-    }
-})
-    .then(res => {
-        res.data = JSON.parse(res.data.replace(/'/g, '"'))
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+// axios({
+//     method: 'POST',
+//     url: 'http://10.42.0.50:5000/unit_category',
+//     data: {
+//         units_taken: ["C", "D"]
+//     }
+// })
+//     .then(res => {
+//         res.data = JSON.parse(res.data.replace(/'/g, '"'))
+//         console.log(res.data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+var data = JSON.parse("{\"nodes\":[{\"ID\":0,\"display\":\"AAAA1111\",\"colour\":\"white\"},{\"ID\":1,\"display\":\"BBBB1111\",\"colour\":\"white\"},{\"ID\":2,\"display\":\"CCCC1111\",\"colour\":\"white\"},{\"ID\":3,\"display\":\"DDDD1111\",\"colour\":\"white\"},{\"ID\":4,\"display\":\"EEEE1111\",\"colour\":\"white\"},{\"ID\":5,\"display\":\"FFFF1111\",\"colour\":\"white\"},{\"ID\":6,\"display\":\"GGGG1111\",\"colour\":\"white\"},{\"ID\":7,\"display\":\"HHHH1111\",\"colour\":\"white\"},{\"ID\":8,\"display\":\"and\",\"colour\":\"blue\"}],\"edges\":[{\"from\":3,\"to\":4,\"colour\":\"black\"},{\"from\":4,\"to\":5,\"colour\":\"white\"},{\"from\":8,\"to\":6,\"colour\":\"white\"},{\"from\":2,\"to\":7,\"colour\":\"white\"},{\"from\":1,\"to\":8,\"colour\":\"blue\"},{\"from\":2,\"to\":8,\"colour\":\"blue\"}]}");
+
+// axios({
+//     method: 'POST',
+//     url: 'http://10.42.0.50:5000/get_all'
+// })
+//     .then(res => {
+//         res.data = JSON.parse(res.data.replace(/'/g, '"'))
+//         console.log(res.data);
+//         data = res.data;
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
 
 
 // axios.get("http://10.42.0.50:5000/unit_category")
