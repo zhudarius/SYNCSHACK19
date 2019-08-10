a0ffd91635f6abb178c3b06108f8322c8f6b929b
Darius Zhu <zhudarius@gmail.com>
2019-08-11 09:12:29 +1000

    Update chatbot.py

--- fetch-preprocess/preprocess/chatbot.py
+++ fetch-preprocess/preprocess/chatbot.py
@@ -1,1 +1,9 @@
-#asdfa
+from mock_2 import *
+@app.route('/compatible_unit/<unit>', methods=['GET', 'POST'])
+def compatible_unit(unit):
+    data_in = request.get_json()
+    categories = calculate_categories(data_in['units_taken'])
+    if unit in categories['can_take']:
+        return str(True), 200
+    else:
+        return str(False), 200
